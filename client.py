735959740d4653f49eba564d6571f1ba1acace5b
Elio Gubser <gubser@users.noreply.github.com>
2015-03-03 16:14:20 +0100

    written udp server and client code.
    to reduce the loss of udp packets, i need consider the return value
    (number of bytes sent) by socket.sendto

--- client.py
+++ client.py
@@ -2,13 +2,15 @@ import socket
 import argparse
 import ipaddress
 import logging
+import time
 #import dtls
 
 class ConnectivityClient:
-    def __init__(self, ips, ports, data, udp_timeout=20):
+    def __init__(self, ips, ports, data, udpTimeout=20, udpPacketSize=4096):
         self.log = logging.getLogger("client")
         self.endpoints = [((ip, port), ipaddress.ip_address(ip).version == 6) for port in ports for ip in ips]
-        self.udp_timeout = udp_timeout
+        self.udpTimeout = udpTimeout
+        self.udpPacketSize = udpPacketSize
         
         self.data = data
         
@@ -16,37 +18,70 @@ class ConnectivityClient:
         for addr, ipv6 in self.endpoints:
             self.run_tcp(addr, ipv6)
             self.run_udp(addr, ipv6)
-            self.run_tls(addr, ipv6)
+            #self.run_tls(addr, ipv6)
             #self.run_dtls()
             #self.run_sctp()
             
     def run_tcp(self, addr, ipv6):
         self.log.info("send image to {} over tcp".format(str(addr)))
-        
-        sock = None
+
         if ipv6:
             sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, socket.IPPROTO_TCP)
         else:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+
         sock.connect(addr)
         sock.send(self.data)
         sock.recv(len(self.data))
         sock.close()
-        
     
     def run_udp(self, addr, ipv6):
-        self.log.info("send image to {} over udp".format(addr))
+        self.log.info("udp {}: sending".format(addr))
+
+        if ipv6:
+            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        
+        for idx in range(0, len(self.data), self.udpPacketSize):
+            packet = self.data[idx:min(idx+self.udpPacketSize, len(self.data))]
+            sock.sendto(packet, addr)
+            time.sleep(0.001)
+
+        self.log.info("udp {}: receiving".format(addr))
+        recvd_length = 0
+        try:
+            start = time.time()
+            sock.settimeout(1)
+            while recvd_length < len(self.data):
+                if start + self.udpTimeout < time.time():
+                    raise TimeoutError
+                try:
+                    recv_addr, recv_data = sock.recvfrom(self.udpPacketSize)
+                except socket.timeout:
+                    pass
+                else:
+                    recvd_length += len(recv_data)
+        except TimeoutError:
+            self.log.info("udp {}: timeout, received {} of {} ({} %)".format(addr, recvd_length, len(self.data), recvd_length*100/len(self.data)))
+        else:
+            self.log.info("udp {}: fully received".format(addr))
+        
+        sock.close()
+        
     
     def run_tls(self, addr, ipv6):
+        raise NotImplemented()
+        
         self.log.info("send image to {} over tls".format(addr))
         
 
 def main():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     
     # parse parameters
     parser = argparse.ArgumentParser(description='Provides a client for TCP/UDP/TLS/DTLS/SCTP connectivity testing.')
-    parser.add_argument('-f', '--file', default='image.jpg', metavar='filename', help='loads image.jpg from current directory.', type=argparse.FileType('rb'), dest='data')
+    parser.add_argument('-f', '--file', default='image.png', metavar='filename', help='loads image.png from current directory.', type=argparse.FileType('rb'), dest='data')
     parser.add_argument('--no-tcpdump-check', action='store_true', dest='no_tcpdump_check')
     parser.add_argument('--hosts', metavar='hostname/ip', type=str, nargs='+', required=True, dest='hosts')
     parser.add_argument('--ports', metavar='port', type=int, nargs='+', required=True, dest='ports')
@@ -66,7 +101,7 @@ def main():
         try:
             ipaddress.ip_address(host)
         except ValueError:
-            infos = socket.getaddrinfo(host, None, family=socket.AF_INET6)
+            infos = socket.getaddrinfo(host, None, family=socket.AF_INET)
             if len(infos) == 0:
                 logging.error("couldn't resolve {}".format(host))
             else:
--- server.py
+++ server.py
@@ -12,21 +12,17 @@ class TcpRequest(socketserver.StreamRequestHandler):
         
         # recv data
         expected_length = len(self.server.data)
-        print("HUHU")
         log.info("new tcp connection".format(self.client_address[0]))
         log.info("recv {} bytes".format(expected_length))
         recvd = self.rfile.read(expected_length)
         if len(recvd) != expected_length:
             log.error("received data not enough")
         
+        # send data
         log.info("send {} bytes".format(expected_length))
         self.wfile.write(self.server.data)
         
         log.info("finished")
-
-class UdpRequest(socketserver.DatagramRequestHandler):
-    def handle(self):
-        pass
         
 class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     address_family = socket.AF_INET6
@@ -34,13 +30,77 @@ class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def server_bind(self):
         self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, False)
         socketserver.TCPServer.server_bind(self)
+
+class UDPServer:
+    def __init__(self, port, ipv6, data, packetSize=4096, timeout=15):
+        self.log = logging.getLogger("udp")
+        
+        self.ipv6 = ipv6
+        if ipv6:
+            self.sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        else:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        
+        self.sock.settimeout(1)
+        
+        self.data = data
+        
+        self.sock.bind(('', port))
+        self.thread = threading.Thread(target=self._proc, daemon=True)
+        
+        self.port = port
+        self.packetSize = packetSize
+        self.timeout = timeout
+        self.requests = {}
+        
+    def start(self):
+        self.thread.start()
+        self.log.info("udp server{} started on port {}".format(" (IPv6)" if self.ipv6 else "", self.port))
     
-class ThreadingUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
-    address_family = socket.AF_INET6
+    def perform_send(self, addr):
+        self.log.info("udp {}:sending data".format(addr))
+        for idx in range(0, len(self.data), self.packetSize):
+            packet = self.data[idx:min(idx+self.packetSize, len(self.data))]
+            self.sock.sendto(packet, addr)
+        self.log.info("udp {}:data send complete".format(addr))
     
-    def server_bind(self):
-        self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, False)
-        socketserver.UDPServer.server_bind(self)
+    def _proc(self):
+        while True:
+            try:
+                buf, addr = self.sock.recvfrom(self.packetSize)
+            except socket.timeout:
+                # check for request timeout, initiate send and remove from requests
+                toDelete = []
+                for key in iter(self.requests):
+                    if self.requests[key][1] + self.timeout < time.time():
+                        self.log.info("udp {}: timeout, initiate sending data".format(key))
+                        self.perform_send(addr)
+                        toDelete.append(addr)
+
+                for key in toDelete:
+                    del self.requests[key]
+
+            else:
+                if addr not in self.requests:
+                    # add new entry: bytes_received, time last received
+                    self.requests[addr] = [0, 0]
+                    
+                    
+                # reset timeout
+                self.requests[addr][1] = time.time()
+                
+                # record bytes received
+                self.requests[addr][0] += len(buf)
+                
+                self.log.debug("udp {}: recvd {} of {} bytes".format(addr, self.requests[addr][0], len(self.data)))
+                
+                # all data received? start transmit
+                if self.requests[addr][0] >= len(self.data):
+                    self.log.info("udp {}: recvd full data".format(addr, self.requests[addr][0], len(self.data)))
+                    self.perform_send(addr)
+                    del self.requests[addr]
+            
+            
 
 def start_servers(ports, data):
     logging.info("starting servers...")
@@ -51,10 +111,8 @@ def start_servers(ports, data):
         tcp.listener_thread = threading.Thread(target=tcp.serve_forever, daemon=True)
         tcp.listener_thread.start()
         
-        udp = ThreadingUDPServer(('', port), UdpRequest)
-        udp.data = data
-        udp.listener_thread = threading.Thread(target=udp.serve_forever, daemon=True)
-        udp.listener_thread.start()
+        udp = UDPServer(port, False, data)
+        udp.start()
                 
         servers[port] = (tcp, udp)
         
@@ -62,11 +120,11 @@ def start_servers(ports, data):
         time.sleep(100)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     
     # parse parameters
     parser = argparse.ArgumentParser(description='Provides a server for TCP/UDP/TLS/DTLS/SCTP connectivity testing.')
-    parser.add_argument('-f', '--file', default='image.jpg', metavar='filename', help='loads image.jpg from current directory.', type=argparse.FileType('rb'), dest='data')
+    parser.add_argument('-f', '--file', default='image.png', metavar='filename', help='loads image.png from current directory.', type=argparse.FileType('rb'), dest='data')
     parser.add_argument('ports', type=int, metavar='port', nargs='+')
 
     args = parser.parse_args()
